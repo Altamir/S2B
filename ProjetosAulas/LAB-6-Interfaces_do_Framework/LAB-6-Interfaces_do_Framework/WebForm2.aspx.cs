164e59b77324d4a18cfb5a450f8d39e4e43db49e
Altamir Benkenstein <altamir.benkenstein@outlook.com>
2013-10-24 16:13:23 -0200

    Usando Dicionario

--- ProjetosAulas/LAB-6-Interfaces_do_Framework/LAB-6-Interfaces_do_Framework/WebForm2.aspx.cs
+++ ProjetosAulas/LAB-6-Interfaces_do_Framework/LAB-6-Interfaces_do_Framework/WebForm2.aspx.cs
@@ -11,6 +11,40 @@ namespace LAB_6_Interfaces_do_Framework
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            UsandoStruct();
+
+            IDictionary<string, Pessoa> dic;
+
+            dic = new Dictionary<string,Pessoa>() ;
+
+            dic.Add("111111111-11", new Pessoa("Carlos",21));
+            dic.Add("222222222-11", new Pessoa("Marcos",41));
+            dic.Add("333333333-11", new Pessoa("MArias",23));
+            dic.Add("444444444-11", new Pessoa("Ana",01));
+
+            Pessoa ps = dic["111111111-11"];
+
+            foreach(var coisa in dic){            
+                Response.Write(coisa.Key);
+                Response.Write(coisa.Value.Nome);
+            }
+
+            var ordena = from p in dic.Values
+                         orderby p.Nome
+                         select p.Nome;
+
+            foreach(var p in ordena){
+                Response.Write(p);
+            }
+
+
+
+
+
+        }
+
+        private void UsandoStruct()
+        {
             PessoaStruct ps;
             ps = new PessoaStruct("Altamir", 29);
             PessoaStruct ps2 = ps;
---
+++ ProjetosAulas/LAB-6-Interfaces_do_Framework/TestPessoaStruct/PessoaStructTest.cs
@@ -0,0 +1,125 @@
+﻿using LAB_6_Interfaces_do_Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Web;
+
+namespace TestPessoaStruct
+{
+    
+    
+    /// <summary>
+    ///This is a test class for PessoaStructTest and is intended
+    ///to contain all PessoaStructTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class PessoaStructTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for PessoaStruct Constructor
+        ///</summary>
+        // TODO: Ensure that the UrlToTest attribute specifies a URL to an ASP.NET page (for example,
+        // http://.../Default.aspx). This is necessary for the unit test to be executed on the web server,
+        // whether you are testing a page, web service, or a WCF service.
+        [TestMethod()]
+        [HostType("ASP.NET")]
+        [AspNetDevelopmentServerHost("C:\\Users\\Altamir Benkenstein\\Dropbox\\GITHUB\\S2B\\ProjetosAulas\\LAB-6-Interfaces_do_Framework\\LAB-6-Interfaces_do_Framework", "/")]
+        [UrlToTest("http://localhost:50891/")]
+        public void PessoaStructConstructorTest()
+        {
+            string n = string.Empty; // TODO: Initialize to an appropriate value
+            int i = 0; // TODO: Initialize to an appropriate value
+            PessoaStruct target = new PessoaStruct(n, i);
+            Assert.Inconclusive("TODO: Implement code to verify target");
+        }
+
+        /// <summary>
+        ///A test for Idade
+        ///</summary>
+        // TODO: Ensure that the UrlToTest attribute specifies a URL to an ASP.NET page (for example,
+        // http://.../Default.aspx). This is necessary for the unit test to be executed on the web server,
+        // whether you are testing a page, web service, or a WCF service.
+        [TestMethod()]
+        [HostType("ASP.NET")]
+        [AspNetDevelopmentServerHost("C:\\Users\\Altamir Benkenstein\\Dropbox\\GITHUB\\S2B\\ProjetosAulas\\LAB-6-Interfaces_do_Framework\\LAB-6-Interfaces_do_Framework", "/")]
+        [UrlToTest("http://localhost:50891/")]
+        public void IdadeTest()
+        {
+            PessoaStruct target = new PessoaStruct(); // TODO: Initialize to an appropriate value
+            int expected = 0; // TODO: Initialize to an appropriate value
+            int actual;
+            target.Idade = expected;
+            actual = target.Idade;
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for Nome
+        ///</summary>
+        // TODO: Ensure that the UrlToTest attribute specifies a URL to an ASP.NET page (for example,
+        // http://.../Default.aspx). This is necessary for the unit test to be executed on the web server,
+        // whether you are testing a page, web service, or a WCF service.
+        [TestMethod()]
+        [HostType("ASP.NET")]
+        [AspNetDevelopmentServerHost("C:\\Users\\Altamir Benkenstein\\Dropbox\\GITHUB\\S2B\\ProjetosAulas\\LAB-6-Interfaces_do_Framework\\LAB-6-Interfaces_do_Framework", "/")]
+        [UrlToTest("http://localhost:50891/")]
+        public void NomeTest()
+        {
+            PessoaStruct target = new PessoaStruct(); // TODO: Initialize to an appropriate value
+            string actual;
+            actual = target.Nome;
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+    }
+}
