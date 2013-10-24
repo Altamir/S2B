f6507930532d6dfe6f03d3260bc3274b430af2e0
Altamir Benkenstein <altamir.benkenstein@outlook.com>
2013-10-24 13:56:34 -0200

    Testando Lab 5 - Interfaces

---
+++ ProjetosAulas/LAB5-Interfaces/LAB5-Interfaces/IEstadoBinario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LAB5_Interfaces
+{
+    public enum EstadoBinario { Ligado, Desligado }
+
+    public interface IEstadoBinario
+    {
+        void Ligar();
+        void Desligar();
+        EstadoBinario Estado { get; }
+
+    }
+}
---
+++ ProjetosAulas/LAB5-Interfaces/LAB5-Interfaces/TermometroEletrico.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LAB5_Interfaces
+{
+    public class TermometroEletrico : Termometro, IEstadoBinario
+    {
+        private bool ligado = false;
+
+        public void Ligar()
+        {
+            ligado = true;
+        }
+        public void Desligar()
+        {
+            ligado = false;
+        }
+        public EstadoBinario Estado
+        {
+            get
+            {
+                if (ligado) return EstadoBinario.Ligado;
+                else return EstadoBinario.Desligado;
+            }
+        }
+
+    }
+}
