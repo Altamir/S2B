360d45ecaa202fe50c265d595a786aebb59f0e65
Altamir Benkenstein <altamir.benkenstein@outlook.com>
2013-10-24 15:03:15 -0200

    FEchando Lab 6

---
+++ ProjetosAulas/LAB-6-Interfaces_do_Framework/LAB-6-Interfaces_do_Framework/PessoaStruct.cs
@@ -0,0 +1,25 @@
+﻿namespace LAB_6_Interfaces_do_Framework
+{
+    public struct PessoaStruct
+    {
+        private string meuNome;
+        private int minhaIdade;
+
+        public PessoaStruct(string n, int i)
+        {
+            meuNome = n;
+            minhaIdade = i;
+        }
+
+        public string Nome
+        {
+            get { return meuNome; }
+        }
+
+        public int Idade
+        {
+            get { return minhaIdade; }
+            set { minhaIdade = value; }
+        }
+    }
+}
---
+++ ProjetosAulas/LAB-6-Interfaces_do_Framework/LAB-6-Interfaces_do_Framework/WebForm2.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LAB_6_Interfaces_do_Framework
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            PessoaStruct ps;
+            ps = new PessoaStruct("Altamir", 29);
+            PessoaStruct ps2 = ps;
+            ps2.Idade = 30;
+            Response.Write(ps.Nome);
+            Response.Write(ps.Idade);
+        }
+    }
+}
