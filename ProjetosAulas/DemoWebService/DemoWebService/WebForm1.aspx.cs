e0363d8c0f2a1d92f32ce18214a0c601ab2b6c79
Altamir Benkenstein <altamir.benkenstein@outlook.com>
2013-10-31 18:26:59 -0200

    commint

---
+++ ProjetosAulas/DemoWebService/DemoWebService/WebForm1.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DemoWebService
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ServiceReference1.GlobalWeatherSoapClient ws = new ServiceReference1.GlobalWeatherSoapClient();
+            Label1.Text = ws.GetCitiesByCountry("brazil");
+        }
+    }
+}
